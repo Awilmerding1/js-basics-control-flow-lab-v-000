c703e31d8d5ee0761c5a39149904aeda16d16182
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-23 18:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet() {
-  
-return 'This one is on me!'
+function scuberGreetingForFeet(distance) {
+  if(distance)
+return 'This one is on me!';
 }
