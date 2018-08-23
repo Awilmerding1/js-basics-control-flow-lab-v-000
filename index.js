fb3f2ddf5d89564dd85dc277a40bc3a17817e89e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-23 19:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ function switchOnCharmFromTip(tip) {
   let response;
   switch(tip) {
     case 'generous':
-    response = "Thank you so much."
+      response = "Thank you so much."
     case 'not as generous':
       response = "Thank you"
+    default
   }
   
   
